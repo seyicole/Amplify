d52e989243bfe6628bc63d647568950dff802a0d
sey cole <sey cole>
2018-07-03 11:59:06 +0100

    first commit

---
+++ assets/js/swipe.js
@@ -0,0 +1,21 @@
+var swiper1 = new Swiper('.swiper1', {
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination1',
+        clickable: true,
+      },
+    });
+    var swiper2 = new Swiper('.swiper2', {
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination2',
+        clickable: true,
+      },
+    });
+    var swiper3 = new Swiper('.swiper3', {
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination3',
+        clickable: true,
+      },
+    });
